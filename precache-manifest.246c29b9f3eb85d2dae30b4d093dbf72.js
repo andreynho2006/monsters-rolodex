1d0ec1485adebf2128546b1af0d0415bf6eabbaf
andreynho2006 <andreynho2006@gmail.com>
2020-02-24 09:59:40 +0200

    Updates

---
+++ precache-manifest.246c29b9f3eb85d2dae30b4d093dbf72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f5f8d9ef3bc7f924c8067abee47f70",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e498c99d23a2c194dabf",
+    "url": "/monsters-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e498c99d23a2c194dabf",
+    "url": "/monsters-rolodex/static/js/main.d8dc283d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
